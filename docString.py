c1a6ca38a1147b5783937ae7bc159ba1a4ce4fa6
arockiajegan <58635230+arockiajegan@users.noreply.github.com>
2023-07-25 16:59:22 +0530

    Create docString.py

---
+++ docString.py
@@ -0,0 +1,12 @@
+#Script Author : AROCKIA JEGAN F
+#visit_me @ www.arcokiajegan.com
+#exp : print(fn.__doc__) - will give you the details
+#import collections
+def docfunction(arg1,arg2=None):
+    '''
+    Hey! This is basic documentation
+    Here you can have your own documentation for your arguments
+    '''
+    #print(arg1,arg2)
+    
+print(docfunction.__doc__)
